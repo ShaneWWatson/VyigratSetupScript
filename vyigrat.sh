bd470509f2d9bec9f73dbe2ebb8223b3cc0c81bb
Shane W Watson <technician2all@gmail.com>
2018-05-21 06:30:30 -0500

    Update vyigrat.sh

--- vyigrat.sh
+++ vyigrat.sh
@@ -1,33 +1,11 @@
-#!/bin/bash
-
-TMP_FOLDER=$(mktemp -d)
-CONFIG_FILE='vyigrat.conf'
-CONFIGFOLDER='/root/.vyigrat'
-DAEMON='vyigratd'
-CLI='vyigrat-cli'
-PATH='/home'
-COIN_NAME='Vyigrat'
-PORT=33133
-RPC_PORT=33134
-
-function setup_node() {
 sudo apt-get -y update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade
 apt-get -y install git automake build-essential libtool autotools-dev autoconf pkg-config libssl-dev libboost-all-dev software-properties-common
 add-apt-repository ppa:bitcoin/bitcoin
 apt-get update
 apt-get -y install libdb4.8-dev libdb4.8++-dev libminiupnpc-dev libevent-dev
-}
-
-function binaries() {
-  echo -e "Downloading Vyigrat binaries"
-  cd /home
-  wget http://vyigrat.com/vyigrat-cli
-  wget http://vyigrat.com/vyigratd
-  chmod +x /home/vyigrat-cli
-  chmod +x /home/vyigratd
-}
-
-# Main code
-clear
-setup_node
-binaries
+echo -e "Downloading Vyigrat binaries"
+cd /home
+wget http://vyigrat.com/vyigrat-cli
+wget http://vyigrat.com/vyigratd
+chmod +x /home/vyigrat-cli
+chmod +x /home/vyigratd
